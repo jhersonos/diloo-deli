ebbc07af48db2eb7bdac3872ca1478f9ea4aedcd
jhersonos <jherson.o.s@hotmail.com>
2017-03-29 16:57:39 -0500

    add street to user

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
+$( document ).ready(function() {
 var login;
 var getuser;
 var adduser;
-window.addEventListener('load', function () {
-	
-		login = new Vue({
+var addstreet;
+
+/*
+$(document).on('click','#agregar',function(){
+	var name = document.getElementById('u-name').value;
+	var phone = document.getElementById('u-phone').value;
+	var email = document.getElementById('u-email').value;
+	$.ajax({
+		type:'POST',
+		url:'http://40.76.4.149:8000/user',
+		dataType:'json',
+		data:{name:name, phone: phone, email:email},
+		headers:{
+			Authorization: 'Bearer '+ window.localStorage.log
+		},
+		success:function(response){
+			console.log(response)
+		},err:function(response){
+			console.log(response)
+		}
+	})
+})
+*/
+
+login = new Vue({
 		el: '#login',
 		data:{
 			token:''
@@ -38,7 +61,7 @@ window.addEventListener('load', function () {
 			list:function(){
 				var phone = document.getElementById('phone').value;
 				this.$http.get('http://40.76.4.149:8000/user/findOne?phone='+phone,{Authorization: 'Bearer '+ window.localStorage.log}).then(response=>{
-					//console.log(response)
+					console.log(response)
 					this.user = response.body.user
 					this.address = response.body.address
 					//script set information user
@@ -59,17 +82,45 @@ window.addEventListener('load', function () {
 		data:{},
 		methods:{
 			add:function(e){
+				console.log('proccesing')
 				e.preventDefault();
-				var name = document.getElementById('u-name').value;
-				var phone = document.getElementById('u-phone').value;
-				var email = document.getElementById('u-email').value;
+					var name = document.getElementById('u-name').value;
+					var phone = document.getElementById('u-phone').value;
+					var email = document.getElementById('u-email').value;
 				this.$http.post('http://40.76.4.149:8000/user', {name: name, phone: phone, email:email}).then((response) => {
 					console.log(response);
+					$('#street').modal('show')
 				},response=>{
 					console.log(response)
+					if(response.status == 500){
+						alert('usuario ya registrado')
+					}else if(response.status == 403){
+						alert('complete formulario o ingrese datos correctos')
+					}
 				});
 			}
 		}
 	})
 
+	addstreet = new Vue({
+		el:'#street',
+		data:{},
+		methods:{
+			addStreet:function(e){
+				e.preventDefault();
+				var phone = document.getElementById('phone').value;
+				this.$http.get('http://40.76.4.149:8000/user/findOne?phone='+phone,{Authorization: 'Bearer '+ window.localStorage.log}).then(response=>{
+					console.log(response.body.user.id);
+				},response=>{
+					console.log(response)
+				})
+			},addStreets:function(){
+				
+			}
+		}
+	});
+//semantic ui
+    $(document).on('click','#Uadd',function(){
+		$('#useradd').modal('show')
+ 	})
 })
--- js/semantic.js
+++ js/semantic.js
@@ -1,11 +1,2 @@
-function init(){
-    $(document).on('click','#Uadd',function(){
-		$('#useradd').modal('show')
- 	})
-}
-
-$( document ).ready(function() {
-	init()
-})
 
 
